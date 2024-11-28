93f65c498839ca84fe23ef6414dd7f097580b9e2
kamalesh777 <ksmaity777@gmail.com>
2024-11-28 20:01:27 +0530

    Fix - testing

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -43,6 +43,7 @@ const Dashboard = () => {
       setValue('name', user?.fullName as string)
       setValue("email", user?.primaryEmailAddress?.emailAddress as string);
       fetchData('/api/view-workspace')
+      fetchData("/api/api-testing");
     }
   }, [isLoaded]);
 
