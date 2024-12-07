47fa8acccd4df27ed6c367d77f9a8d7da9927fa6
kamalesh.maity <kamalesh.maity@codeclouds.in>
2024-12-07 23:39:39 +0530

    Dev - setting page is ready now

--- src/modules/setting/ConfigureForm.tsx
+++ src/modules/setting/ConfigureForm.tsx
@@ -1,8 +1,32 @@
+import ButtonWrapper from '@/components/wrapper/ButtonWrapper'
+import InputFieldWrapper from '@/components/wrapper/InputFieldWrapper'
+import { PRIMARY_DOMAIN } from '@/constants/AppConstant'
+import { Checkbox, FormControlLabel, Grid2, InputAdornment } from '@mui/material'
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 const ConfigureForm = () => {
+
+  const {
+    handleSubmit,
+    formState: { errors },
+    control,
+    setValue,
+  } = useForm({
+    defaultValues: {
+      name: "",
+      email: "",
+      company_name: "",
+      workspace_name: "",
+    },
+  });
   return (
-    <div>ConfigureForm</div>
+    <form>
+      <h3>Control Panel</h3>
+      
+      <FormControlLabel className='d-block' control={<Checkbox defaultChecked />} label="Ready to play?" />
+      <FormControlLabel className='d-block' control={<Checkbox defaultChecked />} label="Stop profile updation" />
+    </form>
   )
 }
 
--- src/modules/setting/index.tsx
+++ src/modules/setting/index.tsx
@@ -1,22 +1,20 @@
-import { Grid2, Card, CardContent, CardHeader, Box, Paper, Tooltip } from '@mui/material'
+import { Grid2, Box, Paper, Checkbox, FormControlLabel } from '@mui/material'
 import React from 'react'
-import ConfigureForm from './ConfigureForm'
 import UserList from './UserList'
-import { HelpOutline } from '@mui/icons-material'
 
 const SettingComp = () => {
   return (
     <div className="company-form">
       <Grid2 container justifyContent="center">
         <Grid2 size={{ md: 8, sm: 12 }}>
-          <Card className='mb-4'>
-            <CardContent title='Control Panel'>
-                <ConfigureForm />
-            </CardContent>
-          </Card>
-          <Box component={"h3"}>All User's <Tooltip title="All users are listed for this current workspace"><HelpOutline fontSize="small" /></Tooltip></Box>
+          <Box component={"h3"}>Control Panel</Box>
+          <form className='mb-3'>
+            <FormControlLabel className='d-block' control={<Checkbox defaultChecked />} label="Ready to play?" />
+            <FormControlLabel className='d-block' control={<Checkbox defaultChecked />} label="Stop profile updation" />
+          </form>
+          <Box component={"p"} sx={{ color: "error.main" }}>All users are listed for this current workspace, Admin can active or deactivate a user. </Box>
             <Paper >
-                <UserList />
+              <UserList />
             </Paper>
         </Grid2>
       </Grid2>
