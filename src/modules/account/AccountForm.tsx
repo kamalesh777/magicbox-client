6cacf8160dc58f9d3b764e3a7631982d3e4c708c
kamalesh777 <ksmaity777@gmail.com>
2024-12-08 20:53:46 +0530

    Fix - account

--- src/modules/account/AccountForm.tsx
+++ src/modules/account/AccountForm.tsx
@@ -9,7 +9,8 @@ import {
 import { RootState } from "@/store/index";
 import { updateUserDetails } from "@/store/slice/userSlice";
 import { useUser } from "@clerk/nextjs";
-import { Box, Grid2 } from "@mui/material";
+import { Box, Grid2, Stack } from "@mui/material";
+import Link from "next/link";
 import React, { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { useSelector, useDispatch } from "react-redux";
@@ -63,91 +64,95 @@ const AccountForm = () => {
   };
 
   return (
-      <form onSubmit={handleSubmit((values) => formSubmitHandler(values))}>
-        <h3>Update Account</h3>
-        <Box component={"p"} sx={{ color: "error.main" }} className="mb-4">
-          *Note: Please provide valid details to avoid any issues later.
-        </Box>
-        <Grid2 container size={12} columnSpacing={2}>
-          <Grid2 size={6}>
-            <InputFieldWrapper
-              label="Name"
-              name="name"
-              control={control}
-              errors={errors}
-              required={true}
-            />
-          </Grid2>
-          <Grid2 size={6}>
-            <InputFieldWrapper
-              label="Email"
-              name="email"
-              control={control}
-              errors={errors}
-              required={true}
-              textFieldProps={{
-                disabled: true,
-              }}
-            />
-          </Grid2>
+    <form onSubmit={handleSubmit((values) => formSubmitHandler(values))}>
+      <h3>Update Account</h3>
+      <Box component={"p"} sx={{ color: "error.main" }} className="mb-4">
+        *Note: Please provide valid details to avoid any issues later.
+      </Box>
+      <Grid2 container size={12} columnSpacing={2}>
+        <Grid2 size={6}>
+          <InputFieldWrapper
+            label="Name"
+            name="name"
+            control={control}
+            errors={errors}
+            required={true}
+          />
         </Grid2>
-        <Grid2 size={12}>
+        <Grid2 size={6}>
           <InputFieldWrapper
-            label="Phone"
-            name="phone"
+            label="Email"
+            name="email"
             control={control}
             errors={errors}
             required={true}
-            rules={{
-              valueAsNumber: true,
-              minLength: {
-                value: 10, // Only allow numbers 0-9
-                message: "Number must be 10 character long",
-              },
-              pattern: {
-                value: /^[0-9]*$/, // Only allow numbers 0-9
-                message: "Number must be between 0 and 9",
-              },
+            textFieldProps={{
+              disabled: true,
             }}
           />
+        </Grid2>
+      </Grid2>
+      <Grid2 size={12}>
+        <InputFieldWrapper
+          label="Phone"
+          name="phone"
+          control={control}
+          errors={errors}
+          required={true}
+          rules={{
+            valueAsNumber: true,
+            minLength: {
+              value: 10, // Only allow numbers 0-9
+              message: "Number must be 10 character long",
+            },
+            pattern: {
+              value: /^[0-9]*$/, // Only allow numbers 0-9
+              message: "Number must be between 0 and 9",
+            },
+          }}
+        />
 
-          <InputFieldWrapper
-            label="Address"
-            name="address"
-            control={control}
-            errors={errors}
-            required={true}
-            textFieldProps={{ multiline: true, row: 4 }}
-          />
+        <InputFieldWrapper
+          label="Address"
+          name="address"
+          control={control}
+          errors={errors}
+          required={true}
+          textFieldProps={{ multiline: true, row: 4 }}
+        />
 
-          <InputFieldWrapper
-            label="State"
-            name="state"
-            control={control}
-            errors={errors}
-            required={true}
-          />
+        <InputFieldWrapper
+          label="State"
+          name="state"
+          control={control}
+          errors={errors}
+          required={true}
+        />
 
-          <InputFieldWrapper
-            label="Pincode"
-            name="pincode"
-            control={control}
-            errors={errors}
-            required={true}
-            rules={{
-              valueAsNumber: true,
-              pattern: {
-                value: /^\d{6}$/, // Exactly 6 digits
-                message: "Pincode must be exactly 6 digits",
-              },
-            }}
-          />
-
+        <InputFieldWrapper
+          label="Pincode"
+          name="pincode"
+          control={control}
+          errors={errors}
+          required={true}
+          rules={{
+            valueAsNumber: true,
+            pattern: {
+              value: /^\d{6}$/, // Exactly 6 digits
+              message: "Pincode must be exactly 6 digits",
+            },
+          }}
+        />
+        <Stack>
+          <Link href="/account" legacyBehavior>
+            <ButtonWrapper>Go Back</ButtonWrapper>
+          </Link>
           <ButtonWrapper loading={buttonLoading} type="submit">
             Update Now!
           </ButtonWrapper>
-        </Grid2>
-      </form>
+        </Stack>
+      </Grid2>
+    </form>
   );
 };
 
