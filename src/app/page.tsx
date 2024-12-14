9f04258e1cb3c61037468c29cd1b1e84e7ebda3c
kamalesh777 <ksmaity777@gmail.com>
2024-12-14 21:04:41 +0530

    fix - root page loader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import PageLoader from '@/components/common/PageLoader'
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { RootState } from '../store';
 
 const RootPage = () => {
+  const isUserStateLoading = useSelector(
+    (state: RootState) => state.user.loading
+  );
   return (
-    <PageLoader />
+    isUserStateLoading ? <PageLoader /> : null
   )
 }
 
