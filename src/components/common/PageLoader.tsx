408717cc40396b23ba8cb536878c7df7f3649715
kamalesh777 <ksmaity777@gmail.com>
2024-12-15 12:33:15 +0530

    If page takes time to load then it will auto reload after 10 sec

--- src/components/common/PageLoader.tsx
+++ src/components/common/PageLoader.tsx
@@ -1,15 +1,28 @@
-import { Box, Grid2, LinearProgress } from '@mui/material';
-import React from 'react'
+import { Box, Grid2, LinearProgress, Stack } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { RefreshOutlined } from "@mui/icons-material";
+import { useRouter } from "next/navigation";
 
-const PageLoader = ({ width='50%' }) => {
+const PageLoader = ({ width = "50%" }) => {
+  const router = useRouter()
+
+  useEffect(() => {
+    setTimeout(() => router.refresh(), 10000);
+  }, []);
+
   return (
-    <Grid2 container alignItems="center" justifyContent="center" className="vh-100 w-100">
+    <Grid2
+      container
+      alignItems="center"
+      justifyContent="center"
+      className="vh-100 w-100"
+    >
       <Box sx={{ width: width }}>
-        <p className='text-center'>Please wait...</p>
+        <Box className="text-center">Please wait... </Box>
         <LinearProgress />
       </Box>
     </Grid2>
   );
-}
+};
 
-export default PageLoader
+export default PageLoader;
