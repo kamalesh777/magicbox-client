0f6bc040141232dcdab0657018f84deeb7ccb774
kamalesh777 <ksmaity777@gmail.com>
2024-12-15 20:25:44 +0530

    dd

--- src/modules/play/index.tsx
+++ src/modules/play/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { SentimentDissatisfied } from "@mui/icons-material";
-import { Grid2, Box } from "@mui/material";
+import { Grid2, Box, Container } from "@mui/material";
 import dynamic from "next/dynamic";
 import React from "react";
 
@@ -10,33 +10,35 @@ const ScratchCard = dynamic(() => import("react-scratchcard-v4"));
 const PlayCard = ({ data }: any) => {
   return data?.length > 0 ? (
     <div className="company-form">
-      <Grid2 container justifyContent="center">
-        {data?.map((user: any) => (
-          <Grid2 size={3}>
-            {
-              <ScratchCard
-                width={280}
-                height={226}
-                image={"/scratch_foreground.png"}
-                finishPercent={85}
-                onComplete={() => console.log("===complete", user)}
-              >
-                <div
-                  style={{
-                    display: "flex",
-                    width: "100%",
-                    height: "100%",
-                    alignItems: "center",
-                    justifyContent: "center",
-                  }}
+      <Container disableGutters>
+        <Grid2 container justifyContent="center">
+          {data?.map((user: any) => (
+            <Grid2 size={3}>
+              {
+                <ScratchCard
+                  width={280}
+                  height={226}
+                  image={"/scratch_foreground.png"}
+                  finishPercent={85}
+                  onComplete={() => console.log("===complete", user)}
                 >
-                  <h1>{user.name}</h1>
-                </div>
-              </ScratchCard>
-            }
-          </Grid2>
-        ))}
-      </Grid2>
+                  <div
+                    style={{
+                      display: "flex",
+                      width: "100%",
+                      height: "100%",
+                      alignItems: "center",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <h1>{user.name}</h1>
+                  </div>
+                </ScratchCard>
+              }
+            </Grid2>
+          ))}
+        </Grid2>
+      </Container>
     </div>
   ) : (
     <Box className="text-center my-5">
